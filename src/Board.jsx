cf17b1a768fb4de6000c0e0efdd2151daf9e3706
fahadbinsiddique <fahad.bin.siddique345@gmail.com>
2026-01-08 00:31:09 +0600

    update

--- src/Board.jsx
+++ src/Board.jsx
@@ -15,9 +15,21 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+  const winner = calculateWinner(squares);
+  console.log(winner);
+  
+  let status;
+
+
+  if (winner) {
+    status = `Winner:${winner}`;
+  } else {
+    status = "Next Player " +  (xIsNext ? "X" : "Y");
+  }
+
   function handClick(index) {
     const nextSquares = squares.slice();
-     if (squares[index]) {
+    if (squares[index] || calculateWinner(squares)) {
       return;
     }
     if (xIsNext) {
@@ -30,6 +42,7 @@ const Board = () => {
   }
   return (
     <>
+    <div>{status}</div>
       <div className="flex">
         <Square value={squares[0]} onSquarClick={() => handClick(0)} />
         <Square value={squares[1]} onSquarClick={() => handClick(1)} />
@@ -51,4 +64,22 @@ const Board = () => {
 
 export default Board;
 
- 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let index = 0; index < lines.length; index++) {
+    const [a, b, c] = lines[index];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
